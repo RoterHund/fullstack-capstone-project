695ef36bcb8096a0470a2e2d3873728e28be2c3e
RoterHund <foxton.alan@gmail.com>
2024-04-08 11:24:50 -0400

    fix for collection

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -1,49 +1,50 @@
-const dotenv = require('dotenv');
 const express = require('express');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
+const app = express();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const pino = require('pino');
 const { body, validationResult } = require('express-validator');
+const connectToDatabase = require('../models/db');
+const router = express.Router();
+const dotenv = require('dotenv');
+const pino = require('pino');  // Import Pino logger
 
-const app = express();
-const logger = pino();
+const logger = pino();  // Create a Pino logger instance
+
+dotenv.config();
+
 const JWT_SECRET = process.env.JWT_SECRET;
 
 router.post('/register', async (req, res) => {
     try {
-        // Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
-         const db = await connectToDatabase();
+        // Task 1: Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
+        const db = await connectToDatabase();
 
         // Task 2: Access MongoDB collection
-         const collection = db.collection('gifts')
+        const collection = db.collection("users");
 
         //Task 3: Check for existing email
-         const existingEmail = await collection.findOne({ email: req.body.email });
-         if (existingEmail) {
-            res.status(404).json({message: 'email already exists'});
-         }
+        const existingEmail = await collection.findOne({ email: req.body.email });
+
         const salt = await bcryptjs.genSalt(10);
         const hash = await bcryptjs.hash(req.body.password, salt);
         const email = req.body.email;
 
-        // Save user details in database
-        const newUser = {
+        //Task 4: Save user details in database
+        const newUser = await collection.insertOne({
             email: req.body.email,
             firstName: req.body.firstName,
             lastName: req.body.lastName,
             password: hash,
             createdAt: new Date(),
-        }
-        const savedUser =  db.collection.insertOne(newUser);
-         // Create JWT authentication with user._id as payload
-         const payload = {
+        });
+
+        const payload = {
             user: {
-                id: savedUser.insertedId
-            }
-         };
-         const authtoken = jwt.sign(payload, JWT_SECRET);
+                id: newUser.insertedId,
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
         logger.info('User registered successfully');
         res.json({authtoken,email});
     } catch (e) {
@@ -52,7 +53,3 @@ router.post('/register', async (req, res) => {
 });
 
 module.exports = router;
-
-
-
-
